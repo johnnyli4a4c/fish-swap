5e64345bf775d56d16297dfaec333ca38dfd8b23
Johnny Li <johnnyli4a4c@gmail.com>
2018-02-10 13:28:01 -0500

    Add theme provider

--- app/src/App.js
+++ app/src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import Login from './Screens/Login.js'
-import Prices from './Screens/Prices.js'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MainMenu from './Components/MainMenu.js';
+import Login from './Screens/Login.js';
+import Prices from './Screens/Prices.js';
 
 const mock_price_data = [];
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <section>
-          <Login />
-        </section>
-        <section>
-          <Prices prices={mock_price_data} />
-        </section>
-      </div>
+      <MuiThemeProvider>
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Fishr Price</h1>
+          </header>
+          <MainMenu />
+          <section>
+            <Login />
+          </section>
+          <section>
+            <Prices prices={mock_price_data} />
+          </section>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
