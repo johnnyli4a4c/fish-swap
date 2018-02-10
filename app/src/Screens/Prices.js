841a9cb8b89709cf38a2c3fbc8b209614c6fe90e
Paige Lim <lim.paige@gmail.com>
2018-02-10 13:52:29 -0500

    Initial table component

--- app/src/Screens/Prices.js
+++ app/src/Screens/Prices.js
@@ -2,6 +2,19 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux'
 
 class Prices extends Component {
+  state = {
+    fixedHeader: true,
+    fixedFooter: true,
+    stripedRows: false,
+    showRowHover: true,
+    selectable: false,
+    multiSelectable: false,
+    enableSelectAll: false,
+    deselectOnClickaway: false,
+    showCheckboxes: false,
+    height: '300px',
+  };
+
   render() {
     const { prices } = this.props;
 
@@ -16,7 +29,41 @@ class Prices extends Component {
           })}
         </ul>
         <p className="Prices-intro">
-          This will eventually be table.
+          <Table
+            height={this.state.height}
+            fixedHeader={this.state.fixedHeader}
+            fixedFooter={this.state.fixedFooter}
+            selectable={this.state.selectable}
+            multiSelectable={this.state.multiSelectable}
+          >
+            <TableHeader
+              displaySelectAll={this.state.showCheckboxes}
+              adjustForCheckbox={this.state.showCheckboxes}
+              enableSelectAll={this.state.enableSelectAll}
+            >
+              <TableRow>
+                <TableHeaderColumn tooltip="The Fish">Fish</TableHeaderColumn>
+                <TableHeaderColumn tooltip="Min Price">Min</TableHeaderColumn>
+                <TableHeaderColumn tooltip="Max Price">Max</TableHeaderColumn>
+                <TableHeaderColumn tooltip="Average Price">Average</TableHeaderColumn>
+                <TableHeaderColumn tooltip="The Country">Country</TableHeaderColumn>
+              </TableRow>
+            </TableHeader>
+            <TableBody
+            displayRowCheckbox={this.state.showCheckboxes}
+            deselectOnClickaway={this.state.deselectOnClickaway}
+            showRowHover={this.state.showRowHover}
+            stripedRows={this.state.stripedRows}
+            >              
+            <TableRow key="0">
+                <TableRowColumn>Salmon</TableRowColumn>
+                <TableRowColumn>$1</TableRowColumn>
+                <TableRowColumn>$3</TableRowColumn>
+                <TableRowColumn>$2</TableRowColumn>
+                <TableRowColumn>Canada</TableRowColumn>
+              </TableRow>
+          </TableBody>
+          </Table>
         </p>
       </div>
     );
