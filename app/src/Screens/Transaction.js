231ee37ebbd3609f33f69e022933068de4915064
AnthonyMa0101 <anthony.ma111@gmail.com>
2018-02-10 20:43:19 -0500

    tracking transactions and forms

---
+++ app/src/Screens/Transaction.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TransactionForm from '../Components/TransactionForm.js'
+
+class Transaction extends Component {
+  render() {
+    return (
+      <div>
+        <TransactionForm>
+        </TransactionForm>
+      </div>
+    );
+  }
+};
+
+export default Transaction;
