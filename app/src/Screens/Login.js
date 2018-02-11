aa54ff9d708d58408752fe59e387b6c74a8aa969
AnthonyMa0101 <anthony.ma111@gmail.com>
2018-02-10 20:30:59 -0500

    login and transaction forms

--- app/src/Screens/Login.js
+++ app/src/Screens/Login.js
@@ -10,7 +10,7 @@ class Login extends Component {
   //   };
 
     // this.handleIsLoggedIn = this.handleIsLoggedIn.bind(this)
-  }
+  // }
 
   // handleIsLoggedIn = (value) => {
   //   this.setState({
