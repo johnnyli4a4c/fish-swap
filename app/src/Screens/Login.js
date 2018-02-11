a0c8ceaa4c7874b8a1a65fe67123dff4ecd5f65e
Johnny Li <johnnyli4a4c@gmail.com>
2018-02-11 13:33:00 -0500

    Fix master

--- app/src/Screens/Login.js
+++ app/src/Screens/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import LoginForm from '../Components/LoginForm.js'
+// import LoginForm from '../Components/LoginForm.js'
 
 class Login extends Component {
 
